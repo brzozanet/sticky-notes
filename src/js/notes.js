5efb6ca434369cc2704eee8da2f0b2f9d6859308
Paweł Brzozowski <pawel@brzoza.net>
2023-04-27 19:50:49 +0200

    Finish basic features

--- src/js/notes.js
+++
@@ -1,9 +0,0 @@
-const STICKY_CLASSLIST =
-  "border-2 border-black-500 w-48 min-h-24 p-4 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500";
-
-export const createStickyNote = (value) => {
-  const stickyEl = document.createElement("div");
-  stickyEl.classList.add(...STICKY_CLASSLIST.split(" "));
-  stickyEl.innerHTML = value;
-  return stickyEl;
-};
